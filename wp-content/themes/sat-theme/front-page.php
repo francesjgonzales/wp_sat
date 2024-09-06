878577094bcb22e7637dad9a4876b484db3cc25e
francesjgonzales <francesj7837@gmail.com>
2024-09-05 18:27:49 -0600

    fixed css in main page

--- wp-content/themes/sat-theme/front-page.php
+++ wp-content/themes/sat-theme/front-page.php
@@ -1,21 +1,31 @@
-
-<?php 
+<?php
 get_header();
 ?>
 
-    <div class="container">
-        <h3 class="flashes text-center" style="color: yellow">
+<div class="container-fluid p-5 mt-5">
+    <div class="row align-items-start">
+        <div class="col">
+            <div class="card-body mb-5">
+                <h2>Student Attendance Tracker (SAT)</h2>
+                <h5>A school management tool for educators</h5>
+                <br>
+                <p>Please select your account type</p>
+                <div>
+                    <a href="{{ url_for('show_create_teacher') }}" class="btn btn" role="button" aria-pressed="true">Teacher</a>
 
-        </h3>
+                    <a href="{{ url_for('show_create_parent') }}" class="ms-5 btn btn" role="button" aria-pressed="true">Parent</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm card">
+            <img src="/wp-content/themes/sat-theme/assets/images/main2.png" alt="mainImg">
+        </div>
     </div>
-
-
-    <div class="container">
-    </div>
-
-    <?php 
-    get_footer();
-    ?>
+</div>
 
 
 
+
+<?php
+get_footer();
+?>
--- wp-content/themes/sat-theme/functions.php
+++ wp-content/themes/sat-theme/functions.php
@@ -1,32 +1,46 @@
 <?php
 
-//add dynamic title tag support
-function sat_theme_support(){
+//add dynamic title tag
+function sat_theme_support()
+{
     add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'sat_theme_support');
 
 //insert css style
-function sat_theme_register_styles(){
+function sat_theme_register_styles()
+{
     $version = wp_get_theme()->get('Version');
-    wp_enqueue_style('sat-css', get_template_directory_uri()."/style.css", array('sat-bootstrap'), $version, 'all'
+    wp_enqueue_style(
+        'sat-css',
+        get_template_directory_uri() . "/style.css",
+        array('sat-bootstrap'),
+        $version,
+        'all'
     );
-    wp_enqueue_style('sat-bootstrap', get_template_directory_uri()."https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css ", array(), '5.3.3', 'all');
-    
+    wp_enqueue_style('sat-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css", array(), '5.3.3', 'all');
 }
 
-add_action( 'wp_enqueue_scripts', 'sat_theme_register_styles');
+add_action('wp_enqueue_scripts', 'sat_theme_register_styles');
 
 //insert javascript
-function sat_theme_register_scripts(){
-    wp_enqueue_script('sat-bootstrap-js', get_template_directory_uri()."https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", array(), '5.3.3', 'all'
+function sat_theme_register_scripts()
+{
+    wp_enqueue_script(
+        'sat-bootstrap-js',
+        get_template_directory_uri() . "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+        array(),
+        '5.3.3',
+        'all'
     );
-    wp_enqueue_script('sat-bootstrap-popper', get_template_directory_uri()."https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js", array(), '2.11.8', 'all'
+    wp_enqueue_script(
+        'sat-bootstrap-popper',
+        get_template_directory_uri() . "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+        array(),
+        '2.11.8',
+        'all'
     );
-
 }
 
-add_action( 'wp_enqueue_scripts', 'sat_theme_register_scripts')
-
-?>
+add_action('wp_enqueue_scripts', 'sat_theme_register_scripts');
